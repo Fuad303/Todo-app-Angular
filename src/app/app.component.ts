aedb815686b71940f9bd3ff5191e80d8a12471e2
Fuad303 <sadiqovfuad8@gmail.com>
2023-02-01 19:32:52 +0400

    Lesson 3:Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  name = "Fuad"
+  items = [
+    {description: "Kahvalti", action: "No"},
+    {description: "Spor", action: "No"},
+    {description: "Fatura", action: "No"}
+  ]
 }
